e76010b6bc11f26d5467c4afee7e77de2788168a
Rhapsoody <shalom.agonglo@outlook.fr>
2021-12-01 16:47:08 +0100

    some changes on the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /*----------------------------------
-*HAMBURGER MENU ENDS
+*HAMBURGER MENU STARTS
 -----------------------------------  */
 const hamburger = document.querySelector('#hamburger')
 const navMenu = document.querySelector('#navMenu')
@@ -21,5 +21,40 @@ navLink.forEach(n => n.addEventListener("click",
 
 /*----------------------------------
 *HAMBURGER MENU ENDS
+-----------------------------------  */
+
+
+/*----------------------------------
+*RIPPLE EFFECT STARTS
+-----------------------------------  */
+
+function createRipple(event) {
+    const button = event.currentTarget;
+  
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+  
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
+    circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
+    circle.classList.add("ripple");
+  
+    const ripple = button.getElementsByClassName("ripple")[0];
+  
+    if (ripple) {
+      ripple.remove();
+    }
+  
+    button.appendChild(circle);
+  }
+  
+  const mybuttons = document.getElementsByTagName("button");
+  for (const buttonn of mybuttons) {
+    buttonn.addEventListener("click", createRipple);
+  }
+
+/*----------------------------------
+*RIPPLE EFFECT ENDS
 -----------------------------------  */
 
